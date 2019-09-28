349871c0e3e850d22a6252f91c871058a4cc7b19
alchemistmatt <matthew.monroe@pnnl.gov>
2019-09-28 14:23:57 -0700

    Add Envo info and batch files

---
+++ docs/Convert_BTO.owl.bat
@@ -0,0 +1,4 @@
+..\bin\debug\OWLDataConverter.exe bto.owl /PK:BTO1
+
+rem Arguments to use in Visual Studio
+rem ..\..\docs\bto.owl /PK:BTO1
---
+++ docs/Convert_Envo.owl.bat
@@ -0,0 +1,4 @@
+..\bin\debug\OWLDataConverter.exe envo.owl /PK:ENVO1
+
+rem Arguments to use in Visual Studio
+rem ..\..\docs\envo.owl /PK:ENVO1
